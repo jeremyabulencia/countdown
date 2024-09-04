d7989e69cf77e0155abb2b83a8cb0f4e3610f7f9
Jeremy <jabulencia@yondu.com>
2024-09-04 13:41:58 +0800

    reconfigured countdown with styles and scripts

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 (function ($) {
     $.fn.countdownTimer = function (options) {
         var settings = $.extend({
-            targetDate: new Date(), 
-            format: "dd hh:mm:ss",
+            targetDate: new Date(),
+            borderStyle: "none",
+            border: "1px solid black",
+            fontStyle: "Arial",
+            fontColor: "black",
             callback: function () {},
         }, options);
         
@@ -10,14 +13,29 @@
             var $this = $(this);
             var timerInterval;
             var days, hours, minutes, seconds;
+            
+            htmlElem = "<ul>"+
+                "<li><span id='dd' class='nums'></span><span>Days</span></li>"+
+                "<li><span id='hh' class='nums'></span><span>Hours</span></li>"+
+                "<li><span id='mm' class='nums'></span><span>Minutes</span></li>"+
+                "<li><span id='ss' class='nums'></span><span>Seconds</span></li>"+
+            "</ul>";
+            $this.append(htmlElem);
+
+            $('li').css('font-style', settings.fontStyle);
+            $('li').css('color', settings.fontColor);
+            $("li .nums").css('border', settings.border);
+            
             function updateTimer() {
                 var now = new Date();
                 var timeDifference = settings.targetDate - now;
                 
                 if (timeDifference <= 0) {
                     clearInterval(timerInterval);
-                    var formattedTime = settings.format.replace("dd", 0).replace("hh", 0).replace("mm", 0).replace("ss", 0);
-                    $this.text(formattedTime);
+                    $('#dd').text(0);
+                    $('#hh').text(0);
+                    $('#mm').text(0);
+                    $('#ss').text(0);
                     settings.callback();
                     return;
                 }
@@ -26,8 +44,11 @@
                 hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
                 minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
                 seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-                var formattedTime = settings.format.replace("dd", days).replace("hh", hours).replace("mm", minutes).replace("ss", seconds);
-                $this.text(formattedTime);
+                
+                $('#dd').text(days);
+                $('#hh').text(hours);
+                $('#mm').text(minutes);
+                $('#ss').text(seconds);
             }
 
             // Start the timer initially
